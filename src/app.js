ea0f8791ba5e18e6f43d9be0a4e232e2dd79a23e
Tyrone <tyrone.onyebuagu@gmail.com>
2019-08-19 13:42:11 -0700

    Removed expense data

--- src/app.js
+++ src/app.js
@@ -14,21 +14,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500, createdAt: 2000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 145000, createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 1000 }));
-
-
-store.dispatch(setTextFilter('water'));
-
-// setTimeout(() => {
-//     store.dispatch(setTextFilter('bill'));
-// }, 3000)
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
-
 
 const jsx = (
     <Provider store = {store}>
